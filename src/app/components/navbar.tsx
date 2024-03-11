ff61ad23ef2e96c393a892169f1770ed9352d65f
John Kyle Salaysay <foursalaysay@gmail.com>
2024-03-12 00:26:30 +0800

    refining page

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,9 +1,45 @@
 import React from 'react'
 
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+  } from "@/components/ui/select"
+
+  
 export default function Header() {
+
+    const scrollToSection = () => {
+        const selectElement = document.getElementById('themeSelector') as HTMLSelectElement | null;
+        
+        if (selectElement) {
+          const selectedValue = selectElement.value;
+          const selectedSection = document.getElementById(selectedValue + '-section');
+      
+          if (selectedSection) {
+            selectedSection.scrollIntoView({ behavior: 'smooth' });
+          }
+        }
+      };
   return (
     <div className='w-screen h-20 justify-between'>
         {/* LOGO HERE */}
+        <h1>JK</h1>
+        <div >
+        <Select>
+            <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="Theme" />
+            </SelectTrigger>
+            <SelectContent id='themeSelector' onChange={scrollToSection}>
+                <SelectItem value="light">Pros</SelectItem>
+                <SelectItem value="dark">Tech Stacks</SelectItem>
+                <SelectItem value="system">Contact Me</SelectItem>
+            </SelectContent>
+        </Select>
+
+        </div>
     </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-        
+        <h1>Hello Wordl</h1>
     </div>
   );
 }
